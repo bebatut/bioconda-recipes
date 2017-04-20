801bb58005b87b730e442df401b7c61f9049320c
Bérénice Batut <bebatut@gmail.com>
2017-04-20 21:06:28 +0200

    Add comments for build

--- recipes/phymmbl/build.sh
+++ recipes/phymmbl/build.sh
@@ -3,16 +3,19 @@
 mkdir -p $PREFIX/bin
 mkdir -p $PREFIX/data
 
+# Copy scripts
 cp *.pl $PREFIX/bin
 cp .scripts/*.pl $PREFIX/bin
 cp .scripts/.taxonomyInfo/*.pl $PREFIX/bin
 chmod +x $PREFIX/bin/*.pl
 
+# Fix scripts
 ls $PREFIX/bin/*.pl | xargs sed -i.bak -e '1 s|^.*$|#!/usr/bin/env perl|g'
 ls $PREFIX/bin/*.pl | xargs sed -i.bak -e 's|.scripts/||g'
 ls $PREFIX/bin/*.pl | xargs sed -i.bak -e 's|.icmCode/bin/||g'
 ls $PREFIX/bin/*.pl | xargs sed -i.bak -e 's|.taxonomyInfo/||g'
 
+# Compile sources
 cd .scripts/.icmCode
 tar xzf glimmer3_plusSS.tgz
 ls
@@ -26,5 +29,6 @@ cp bin/* $PREFIX/bin
 cd ../../
 pwd
 
+# Copy taxonomic data
 cp -r .taxonomyData/* $PREFIX/data
 ls $PREFIX/bin/*.pl | xargs sed -i.bak -e 's|.taxonomyData/|../data/|g'
